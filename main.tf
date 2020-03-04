ea3ff2f7067eb68e9363ba1e8e892d4483ad6130
Daiki Horiyama <d19921207@gmail.com>
2020-03-05 02:04:43 +0900

    block public access

--- main.tf
+++ main.tf
@@ -20,6 +20,14 @@ resource "aws_s3_bucket" "private" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.private.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
 output "mykey_arn" {
   value = aws_kms_key.mykey.arn
 }
