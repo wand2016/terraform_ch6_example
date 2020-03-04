903cafa50b177a36f083fb7ba3bb51ff54fe19ce
Daiki Horiyama <d19921207@gmail.com>
2020-03-05 02:34:39 +0900

    バケットポリシー追加

--- main.tf
+++ main.tf
@@ -52,6 +52,24 @@ resource "aws_s3_bucket" "alb_log" {
   }
 }
 
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
+
 output "mykey_arn" {
   value = aws_kms_key.mykey.arn
 }
