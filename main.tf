f1adb7fb92b330109a8b211377afb006e593342e
Daiki Horiyama <d19921207@gmail.com>
2020-03-05 01:53:59 +0900

    SSE-KMSでS3バケット作成

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "private" {
   bucket = "terraform-practice-wand-20200305"
 
@@ -8,8 +13,13 @@ resource "aws_s3_bucket" "private" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm = "aws:kms"
       }
     }
   }
+}
+
+output "mykey_arn" {
+  value = aws_kms_key.mykey.arn
 }
